c350d12743ce5db275a303d9f5d9916a256a4f60
MohamedLalji <lalji7000@gmail.com>
2023-07-31 02:48:57 +0300

    Extra /n bug fixed and syntax for list comprehension added

--- TO-DO LIST/main.py
+++ TO-DO LIST/main.py
@@ -21,6 +21,8 @@ while True:
             todos = file.readlines()
             file.close()
 
+            # new_todos = [item.strip('\n') for item in todos]
+
             for index, item in enumerate(todos):
                 item = item.strip('\n')
                 row = f"{index + 1}-{item}"
