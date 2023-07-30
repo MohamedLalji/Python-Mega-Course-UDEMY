f95ccf57c6c8d5b26f706db365300ca5dd7aad96
MohamedLalji <lalji7000@gmail.com>
2023-07-30 16:28:09 +0300

    Coding exercise 5 added

---
+++ Coding Exercises/Day 6/Coding Exercise 5.py
@@ -0,0 +1,11 @@
+new_member = input("Add a new member: ")
+
+file = open('members.txt', 'r')
+members = file.readlines()
+file.close()
+
+members.append(new_member)
+
+file = open('members.txt', 'w')
+members = file.writelines(members)
+file.close()
