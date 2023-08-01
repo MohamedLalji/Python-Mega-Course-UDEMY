92ab6abc185b419bd37406b9b18c3046209ce51f
MohamedLalji <lalji7000@gmail.com>
2023-08-02 02:21:50 +0300

    Changed from using case to using if else conditional blocks

--- TO-DO LIST/main.py
+++ TO-DO LIST/main.py
@@ -3,57 +3,56 @@ while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo = input("Enter a todo: ") + "\n"
+    if 'add' in user_action:
+        todo = input("Enter a todo: ") + "\n"
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            todos.append(todo)
+        todos.append(todo)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case 'show':
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+    if 'show' in user_action:
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # new_todos = [item.strip('\n') for item in todos]
+        # new_todos = [item.strip('\n') for item in todos]
 
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                row = f"{index + 1}-{item}"
-                print(row)
-        case 'edit':
-            number = int(input("Number of the todo to edit: "))
-            number = number - 1
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+    if 'edit' in user_action:
+        number = int(input("Number of the todo to edit: "))
+        number = number - 1
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            new_todo = input("Enter a new todo: ")
-            todos[number] = new_todo + '\n'
+        new_todo = input("Enter a new todo: ")
+        todos[number] = new_todo + '\n'
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case'complete':
-            number = int(input("Number of the todo to complete: "))
+    if 'complete' in user_action:
+        number = int(input("Number of the todo to complete: "))
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            index = number - 1      # This is done to make it easier and not have to do the calc again and again
-            todo_to_remove = todos[index].strip('\n')
-            todos.pop(index)
+        index = number - 1      # This is done to make it easier and not have to do the calc again and again
+        todo_to_remove = todos[index].strip('\n')
+        todos.pop(index)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-            print(f"Todo {todo_to_remove} was removed from the list.")
+        print(f"Todo {todo_to_remove} was removed from the list.")
 
-        case 'exit':
-            break
+    if 'exit' in user_action:
+        break
 
 print("Bye!")
