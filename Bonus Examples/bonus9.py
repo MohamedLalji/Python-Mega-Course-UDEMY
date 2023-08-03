9b019a7a4e2c6daa5bde7ffabadc890a7d26ce1a
MohamedLalji <lalji7000@gmail.com>
2023-08-04 02:36:33 +0300

    bonus 9 added... Password Strength checking app

---
+++ Bonus Examples/bonus9.py
@@ -0,0 +1,30 @@
+# Application That checks the strength of a password
+
+password = input("Enter new Password: ")
+result = []
+
+if len(password) >= 8:
+    result.append(True)
+else:
+    result.append(False)
+
+digit = False
+for i in password:
+    if i.isdigit():
+        digit = True
+
+result.append(digit)
+
+uppercase = False
+for i in password:
+    if i.isupper():
+        uppercase = True
+
+result.append(uppercase)
+
+if all(result):
+    print("Strong Password")
+else:
+    print("Weak Password")
+
+
